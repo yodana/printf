290611487648bd96a089746a7421a2ccc63e982d
Yohann DANA <yodana@e2r3p1.42.fr>
2019-04-14 23:01:46 +0200

    avancement f

--- ft_conv_f.c
+++ ft_conv_f.c
@@ -60,31 +60,64 @@ char *ft_calc_i_bi(int nbr)
     bi = ft_strrev_fr(bi);
     return (bi);
 }
+
+double         ft_power(double nbr, double power)
+{
+    int i;
+    double res;
+
+    res = 1;
+    i = 1;
+    while (i <= power)
+    {
+        res = res * nbr;
+        i++;
+    }
+    return (res);
+}
+
 #include <math.h>
 double     ft_bi_to_dec(char *m)
 {
     int i;
     double res;
-    int power = 1;
+    double power = 1;
     i = 0;
     res = 0;
     while(m[i])
     {
-        res = res + ((m[i] - '0') * pow(2,-power));
+        res = res + ((m[i] - '0') * (1 / ft_power(2, power)));
         i++;
         power++;
     }
-    printf("mantisse ? %.64f\n",res);
+    printf("mantisse ? %f\n",res);
     return (res);
 }
+
+
+void ft_dtoa(double nbr, int size)
+{
+    int i;
+
+    i = 0;
+    while (nbr > 1)
+        nbr = nbr / 10;
+    printf("float test = %f\n",nbr);
+    while (i != size)
+    {
+        nbr = nbr * 10;
+        printf("float dans while = %f\n",nbr);
+        i++;
+    }
+}
+
 int    ft_conv_f(va_list args,int flags, void(*display)(long long))
 {
     double i;
     (void)flags;
     (void)(*display);
-    char *i_part;
-    char *d_part;
     t_float *f;
+    char *bi_part;
     f = (t_float*)malloc(sizeof(t_float));
     i = va_arg(args, double);
     int d = (int)i;
@@ -92,22 +125,22 @@ int    ft_conv_f(va_list args,int flags, void(*display)(long long))
     i = i - (double)d;
     printf("| re == %f\n",i);
     printf("%s\n",ft_calc_i_bi(d));
-    i_part = ft_calc_i_bi(d);
     printf("%s\n",ft_calc_d_bi(i));
-    d_part = ft_calc_d_bi(i);
-    char *bi_part;
-    bi_part = malloc(sizeof(char) * (ft_strlen(i_part) + ft_strlen(d_part)) + 1);
-    bi_part = ft_strjoin(i_part,d_part);
-    char *exposant;
-    exposant = ft_calc_i_bi(1023 + ft_strlen(i_part) - 1);
+    bi_part = ft_strjoin(ft_calc_i_bi(d),ft_calc_d_bi(i));
+    int exposant;
+    exposant = 1023 - ft_strlen(ft_calc_i_bi(d)) - 1;
     f->mantisse = &bi_part[1];
     //ft_mantisse(bi_part);
         printf("toute la partie binaore == %s\n",bi_part);
         printf("matisse part == %s\n",f->mantisse);
-        printf("exposant == %s\n",exposant);
+        printf("exposant == %d\n",exposant);
    double res;
    res = ft_bi_to_dec(f->mantisse);
+   printf("test power = %f\n",(1 / (double)ft_power(2,3)));
+   printf("reel power = %f\n",(1 / (double)pow(2,3)));
    double res_final;
-   res_final = (1 + res) * pow(2,7);
+   res_final = (1 + res) * ft_power(2,ft_strlen(ft_calc_i_bi(d)) - 1);
+   printf("%s\n",ft_itoa(res_final));
+   //ft_dtoa(res_final,64);
     return (0);
 }
